ced9e9fad30017b7d08bfb06d14361c895b618b5
Fernando Lopes <87499075+einand0@users.noreply.github.com>
2025-04-03 21:26:18 -0300

    feat: UserRepository criado

---
+++ src/main/java/com/einando/taskmanager/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.einando.taskmanager.repositories;
+
+import com.einando.taskmanager.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByEmail(String email);
+}
