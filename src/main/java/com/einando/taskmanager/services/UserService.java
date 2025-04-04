e2cdaa1237bcd60d0d564cdf9332ec8f4dc9f2f0
Fernando Lopes <87499075+einand0@users.noreply.github.com>
2025-04-03 23:20:46 -0300

    feat:User controller e service criados, DTO response criado

---
+++ src/main/java/com/einando/taskmanager/services/UserService.java
@@ -0,0 +1,21 @@
+package com.einando.taskmanager.services;
+
+import com.einando.taskmanager.dto.UserCreateDTO;
+import com.einando.taskmanager.dto.UserResponseDTO;
+import com.einando.taskmanager.entities.User;
+import com.einando.taskmanager.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    public UserRepository userRepository;
+
+    public UserResponseDTO createUser(UserCreateDTO dto){
+        User newUser = new User(dto);
+        userRepository.save(newUser);
+        return new UserResponseDTO(newUser);
+    }
+}
